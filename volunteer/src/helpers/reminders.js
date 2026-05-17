e976647fef9b58f5ea46cc72f4c62d9753571194
Abhay Raina <YOUR_EMAIL@example.com>
2026-05-17 12:37:22 -0400

    Remove volunteer system

--- volunteer/src/helpers/reminders.js
+++
@@ -1,6 +0,0 @@
-export const DEFAULT_REMINDER_TEMPLATE =
-  'Hi {{name}}, this is a reminder for {{topic}} on {{date}} at {{time}}. Reply to your coordinator if anything changes.';
-
-export function renderReminderPreview(template, context) {
-  return template.replace(/\{\{(\w+)\}\}/g, (_, key) => context[key] ?? '');
-}
